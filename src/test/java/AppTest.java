aab87fd880617fc498421e5f6090e29c367ae8fd
kevin <mattisonii@yahoo.com>
2016-02-19 18:11:04 -0800

    Pass integration spec.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,27 +1,27 @@
-// import org.junit.*;
-// import static org.junit.Assert.*;
-// import java.util.ArrayList;
-// import org.fluentlenium.adapter.FluentTest;
-// import org.junit.ClassRule;
-// import org.junit.Test;
-// import org.openqa.selenium.WebDriver;
-// import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-// import static org.assertj.core.api.Assertions.assertThat;
-//
-// public class AppTest extends FluentTest {
-//   public WebDriver webDriver = new HtmlUnitDriver();
-//   public WebDriver getDefaultDriver() {
-//       return webDriver;
-//   }
-//
-//   @ClassRule
-//   public static ServerRule server = new ServerRule();
-//
-//     @Test
-//     public void rootTest() {
-//         goTo("http://localhost:4567/");
-//         assertThat(pageSource()).contains("Create-a-dictionary");
-//     }
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+    @Test
+    public void rootTest() {
+        goTo("http://localhost:4567/");
+        assertThat(pageSource()).contains("Create-a-dictionary");
+    }
 //
 //     @Test
 //       public void vocabWordIsCreated() {
@@ -30,4 +30,4 @@
 //         submit(".btn");
 //         assertThat(pageSource()).contains("Your task has been saved.");
 //       }
-// }
+}
